c715abced93dae1b9fd4aef7bcfdba7ba7e0577d
Xavier <xavier.chong.j@gmail.com>
2021-05-11 07:13:08 -0400

    Improve player card scene design and binding

--- src/main/kotlin/com/helloworldramen/tripletriad/tripletriad/scenes/card/PlayerCardScene.kt
+++ src/main/kotlin/com/helloworldramen/tripletriad/tripletriad/scenes/card/PlayerCardScene.kt
@@ -4,6 +4,7 @@ import com.helloworldramen.tripletriad.tripletriad.models.Card
 import com.helloworldramen.tripletriad.tripletriad.models.CardRarity
 import com.helloworldramen.tripletriad.tripletriad.models.CardType
 import com.helloworldramen.tripletriad.tripletriad.models.PlayerCard
+import godot.AspectRatioContainer
 import godot.ColorRect
 import godot.Label
 import godot.Spatial
@@ -16,59 +17,80 @@ import godot.global.GD
 @RegisterClass
 class PlayerCardScene: Spatial() {
 
-	private val backgroundColorRect: ColorRect by lazy { getNodeAs("Container/BackgroundColorRect")!! }
-	private val rarityLabel: Label by lazy { getNodeAs("Container/BackgroundColorRect/RarityLabel")!! }
-	private val typeLabel: Label by lazy { getNodeAs("Container/BackgroundColorRect/TypeLabel")!! }
-	private val nameLabel: Label by lazy { getNodeAs("Container/BackgroundColorRect/NameLabel")!! }
-	private val northLabel: Label by lazy { getNodeAs("Container/BackgroundColorRect/NorthLabel")!! }
-	private val eastLabel: Label by lazy { getNodeAs("Container/BackgroundColorRect/EastLabel")!! }
-	private val southLabel: Label by lazy { getNodeAs("Container/BackgroundColorRect/SouthLabel")!! }
-	private val westLabel: Label by lazy { getNodeAs("Container/BackgroundColorRect/WestLabel")!! }
+	private val container: AspectRatioContainer by lazy {getNodeAs("Container")!! }
+	private val colorRect: ColorRect by lazy { getNodeAs(colorRectPath())!! }
+	private val rarityLabel: Label by lazy { getNodeAs(colorRectPath("RarityLabel"))!! }
+	private val typeLabel: Label by lazy { getNodeAs(colorRectPath("TypeLabel"))!! }
+	private val nameLabel: Label by lazy { getNodeAs(colorRectPath("NameLabel"))!! }
+	private val northLabel: Label by lazy { getNodeAs(colorRectPath("NorthLabel"))!! }
+	private val eastLabel: Label by lazy { getNodeAs(colorRectPath("EastLabel"))!! }
+	private val southLabel: Label by lazy { getNodeAs(colorRectPath("SouthLabel"))!! }
+	private val westLabel: Label by lazy { getNodeAs(colorRectPath("WestLabel"))!! }
+
+	private fun colorRectPath(path: String = ""): String {
+		return "Container/MarginContainer/ColorRect${if (path.isEmpty()) "" else "/$path"}"
+	}
 
 	@RegisterFunction
 	override fun _ready() {
 		GD.print("Hello world!")
 
-		val playerCard = PlayerCard(card = Card.Amaljaa, playerId = 0, isPlayable = true)
+		val playerCard = PlayerCard(card = Card.AlexanderPrime, playerId = 0, isPlayable = false)
 		bind(playerCard)
 	}
 
 	fun bind(playerCard: PlayerCard?) {
-		rarityLabel.text = when(playerCard?.card?.rarity) {
-			CardRarity.Common -> "*"
-			CardRarity.Uncommon -> "**"
-			CardRarity.Rare -> "***"
-			CardRarity.Epic -> "****"
-			CardRarity.Legendary -> "*****"
-			else -> ""
+		if (playerCard?.card == Card.UNKNOWN) {
+			bindUnknown()
+		} else {
+			rarityLabel.text = when(playerCard?.card?.rarity) {
+				CardRarity.Common -> "*"
+				CardRarity.Uncommon -> "**"
+				CardRarity.Rare -> "***"
+				CardRarity.Epic -> "****"
+				CardRarity.Legendary -> "*****"
+				else -> "?????"
+			}
+
+			typeLabel.text = when(playerCard?.card?.type) {
+				CardType.Beastman -> "B"
+				CardType.Garland -> "G"
+				CardType.Primal -> "P"
+				CardType.Scions -> "S"
+				else -> ""
+			}
+
+			nameLabel.text = playerCard?.card?.name?.take(7) ?: "???????"
+
+			northLabel.text = playerCard?.n().getValueString()
+			eastLabel.text = playerCard?.e().getValueString()
+			southLabel.text = playerCard?.s().getValueString()
+			westLabel.text = playerCard?.w().getValueString()
 		}
 
-		typeLabel.text = when(playerCard?.card?.type) {
-			CardType.Beastman -> "B"
-			CardType.Garland -> "G"
-			CardType.Primal -> "P"
-			CardType.Scions -> "S"
-			else -> ""
-		}
-
-		nameLabel.text = playerCard?.card?.name?.take(7) ?: ""
-
-		northLabel.text = playerCard?.n().getValueString()
-		eastLabel.text = playerCard?.e().getValueString()
-		southLabel.text = playerCard?.s().getValueString()
-		westLabel.text = playerCard?.w().getValueString()
-
-		backgroundColorRect.color = when {
+		colorRect.color = when {
 			playerCard?.isPlayable == false -> Color.gray
 			playerCard?.playerId == 0 -> Color.cadetblue
-			playerCard?.playerId == 1 -> Color.chartreuse
-			else -> Color.transparent
+			playerCard?.playerId == 1 -> Color.mediumvioletred
+			else -> Color.black
 		}
+
+		if (playerCard == null) container.hide() else container.show()
+	}
+
+	private fun bindUnknown() {
+		rarityLabel.text = ""
+		typeLabel.text = ""
+		nameLabel.text = ""
+		northLabel.text = ""
+		eastLabel.text = ""
+		southLabel.text = ""
+		westLabel.text = ""
 	}
 
 	private fun Int?.getValueString(): String {
 		return when {
-			this == null -> ""
+			this == null -> "?"
 			this >= 10 -> "A"
 			this <= 0 -> "1"
 			else -> this.toString()
